adb6ae3ab92e46df04f26d2247c2f2ed026efa92
Chapin Bryce <mail@chapinb.com>
2022-12-04 09:05:58 -0500

    Add helper to get all ips from data

--- src/util/mod.rs
+++ src/util/mod.rs
@@ -2,7 +2,13 @@ use lazy_static::lazy_static;
 use regex::Regex;
 use std::net::{IpAddr, Ipv4Addr, Ipv6Addr};
 
-pub fn get_all_ipv4(data: &str) -> Vec<Ipv4Addr> {
+pub fn get_all_ips(data: &str) -> Vec<IpAddr> {
+    let mut all_ips: Vec<IpAddr> = get_all_ipv4(&data);
+    all_ips.append(&mut get_all_ipv6(&data));
+    all_ips
+}
+
+pub fn get_all_ipv4(data: &str) -> Vec<IpAddr> {
     lazy_static! {
         static ref IPV4: Regex = Regex::new(
             r"(?:(?:(?:25[0-5]|(?:2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(?:25[0-5]|(?:2[0-4]|1{0,1}[0-9]){0,1}[0-9]))"
@@ -10,16 +16,15 @@ pub fn get_all_ipv4(data: &str) -> Vec<Ipv4Addr> {
     }
     IPV4.find_iter(data)
         .map(|x| {
-            println!("{x:#?}");
             let substr = x.as_str();
             substr.parse::<Ipv4Addr>().ok()
         })
         .filter(|x| x.is_some())
-        .map(|x| x.unwrap())
+        .map(|x| IpAddr::V4(x.unwrap()))
         .collect::<Vec<_>>()
 }
 
-fn get_all_ipv6(data: &str) -> Vec<Ipv6Addr> {
+fn get_all_ipv6(data: &str) -> Vec<IpAddr> {
     lazy_static! {
         static ref IPV6: Regex = Regex::new(
     r"(?:(?:(?:(?:[0-9a-fA-F]){1,4}):){1,4}:[^\s:](?:(?:(?:25[0-5]|(?:2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(?:25[0-5]|(?:2[0-4]|1{0,1}[0-9]){0,1}[0-9])))|(?:::(?:ffff(?::0{1,4}){0,1}:){0,1}[^\s:](?:(?:(?:25[0-5]|(?:2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(?:25[0-5]|(?:2[0-4]|1{0,1}[0-9]){0,1}[0-9])))|(?:fe80:(?::(?:(?:[0-9a-fA-F]){1,4})){0,4}%[0-9a-zA-Z]{1,})|(?::(?:(?::(?:(?:[0-9a-fA-F]){1,4})){1,7}|:))|(?:(?:(?:[0-9a-fA-F]){1,4}):(?:(?::(?:(?:[0-9a-fA-F]){1,4})){1,6}))|(?:(?:(?:(?:[0-9a-fA-F]){1,4}):){1,2}(?::(?:(?:[0-9a-fA-F]){1,4})){1,5})|(?:(?:(?:(?:[0-9a-fA-F]){1,4}):){1,3}(?::(?:(?:[0-9a-fA-F]){1,4})){1,4})|(?:(?:(?:(?:[0-9a-fA-F]){1,4}):){1,4}(?::(?:(?:[0-9a-fA-F]){1,4})){1,3})|(?:(?:(?:(?:[0-9a-fA-F]){1,4}):){1,5}(?::(?:(?:[0-9a-fA-F]){1,4})){1,2})|(?:(?:(?:(?:[0-9a-fA-F]){1,4}):){1,6}:(?:(?:[0-9a-fA-F]){1,4}))|(?:(?:(?:(?:[0-9a-fA-F]){1,4}):){1,7}:)|(?:(?:(?:(?:[0-9a-fA-F]){1,4}):){7,7}(?:(?:[0-9a-fA-F]){1,4}))"
@@ -28,12 +33,11 @@ fn get_all_ipv6(data: &str) -> Vec<Ipv6Addr> {
 
     IPV6.find_iter(data)
         .map(|x| {
-            println!("{x:#?}");
             let substr = x.as_str();
             substr.parse::<Ipv6Addr>().ok()
         })
         .filter(|x| x.is_some())
-        .map(|x| x.unwrap())
+        .map(|x| IpAddr::V6(x.unwrap()))
         .collect::<Vec<_>>()
 }
 
@@ -44,13 +48,13 @@ mod test {
     #[test]
     fn test_get_all_ipv4() {
         let tests = vec![
-            ("8.8.8.8", vec![Ipv4Addr::new(8, 8, 8, 8)]),
+            ("8.8.8.8", vec![IpAddr::V4(Ipv4Addr::new(8, 8, 8, 8))]),
             (
                 "some127.0.0.1data8.8.8.8here10.100.27.43",
                 vec![
-                    Ipv4Addr::new(127, 0, 0, 1),
-                    Ipv4Addr::new(8, 8, 8, 8),
-                    Ipv4Addr::new(10, 100, 27, 43),
+                    IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)),
+                    IpAddr::V4(Ipv4Addr::new(8, 8, 8, 8)),
+                    IpAddr::V4(Ipv4Addr::new(10, 100, 27, 43)),
                 ],
             ),
         ];
@@ -66,15 +70,17 @@ mod test {
         let tests = vec![
             (
                 "2001:4860:4860::8844",
-                vec![Ipv6Addr::new(
+                vec![IpAddr::V6(Ipv6Addr::new(
                     0x2001, 0x4860, 0x4860, 0x0, 0x0, 0x0, 0x0, 0x8844,
-                )],
+                ))],
             ),
             (
                 "2620:119:53::53,2606:4700:4700::111",
                 vec![
-                    Ipv6Addr::new(0x2620, 0x119, 0x53, 0x0, 0x0, 0x0, 0x0, 0x53),
-                    Ipv6Addr::new(0x2606, 0x4700, 0x4700, 0x0, 0x0, 0x0, 0x0, 0x111),
+                    IpAddr::V6(Ipv6Addr::new(0x2620, 0x119, 0x53, 0x0, 0x0, 0x0, 0x0, 0x53)),
+                    IpAddr::V6(Ipv6Addr::new(
+                        0x2606, 0x4700, 0x4700, 0x0, 0x0, 0x0, 0x0, 0x111,
+                    )),
                 ],
             ),
         ];
