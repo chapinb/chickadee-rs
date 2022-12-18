c1921e083757cef394be75fc7b41d9913abc6d50
Chapin Bryce <mail@chapinb.com>
2022-12-18 09:00:34 -0500

    F!! Initial resolver logic for column selection

--- src/resolver/ip_api.rs
+++ src/resolver/ip_api.rs
@@ -32,13 +32,68 @@ pub struct IpApiRecord {
     pub hosting: bool,
 }
 
-pub struct Resolve {
+pub struct Resolver {
     pub ips: Vec<IpAddr>,
-    results: IpApiRecords,
-    client: Client,
+    pub columns: Vec<String>,
+}
+
+impl Resolver {
+    fn new(ips: Vec<IpAddr>, columns: Vec<String>) -> Resolver {
+        return Resolver {ips, columns};
+    }
+
+    fn check_columns(requested_columns: Vec<String>) -> Vec<String> {
+        let allowed = Resolver::allowed_columns();
+        let selected = requested_columns
+            .into_iter()
+            .filter(|column| allowed.contains(column))
+            .collect();
+        return selected;
+    }
+
+    fn allowed_columns() -> Vec<String> {
+        return vec![
+            String::from("query"),
+            String::from("status"),
+            String::from("continent"),
+            String::from("continent_code"),
+            String::from("country"),
+            String::from("country_code"),
+            String::from("region"),
+            String::from("region_name"),
+            String::from("city"),
+            String::from("district"),
+            String::from("zip"),
+            String::from("lat"),
+            String::from("lon"),
+            String::from("timezone"),
+            String::from("offset"),
+            String::from("currency"),
+            String::from("isp"),
+            String::from("org"),
+            String::from("asn"),
+            String::from("asname"),
+            String::from("mobile"),
+            String::from("proxy"),
+            String::from("hosting"),
+        ];
+    }
 }
 
 #[cfg(test)]
 mod test {
     use super::*;
+    use std::net::Ipv4Addr;
+
+    #[test]
+    fn test_cols_allowed() {
+        let sample = vec![
+            String::from("query"),
+            String::from("status"),
+            String::from("lat"),
+            String::from("lon"),
+        ];
+        let actual = Resolver::check_columns(sample.to_owned());
+        assert_eq!(sample, actual);
+    }
 }
